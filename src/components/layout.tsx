bce8c556f1863502ae4be02af9d54919ef333702
Vinicius Mota <vinicius.motamorici@gmail.com>
2024-10-29 20:59:15 -0300

    ADD TAGUEAMENTO

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,11 +2,13 @@
 import { Header } from './header';
 import { Footer } from './footer';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export function Layout() {
   return (
     <div className="min-h-screen">
+      <Analytics/>
       <Header />
       <main className="flex-1 p-4 self-center ">
         <Outlet/>
